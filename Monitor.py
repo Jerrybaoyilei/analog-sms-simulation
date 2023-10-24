9b2da52bcd1a324d6b21c9b74c9832fc2460daf1
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-10-24 02:09:59 -0400

    Added unit testing for monitor.py. Still trying to figure out how to test for print output

--- Monitor.py
+++ Monitor.py
@@ -13,7 +13,10 @@ class Monitor:
         return self.update_interval
 
     def set_update_interval(self, update_interval):
-        self.update_interval = update_interval
+        if update_interval > 0 and isinstance(update_interval, int):
+            self.update_interval = update_interval
+        else:
+            print("Update interval must be a positive integer.")
 
     def record(self, report_counter, total_num_messages):
         total_num_successes = sum([sender.get_success_counter()
@@ -26,13 +29,15 @@ class Monitor:
         else:
             overall_average_time_per_message = sum(
                 [sender.get_total_time() for sender in self.senders_list]) / total_num_successes
-        print("================================")
-        print(f"Report #{report_counter}\n")
-        print(f"Total number of successes: {total_num_successes}\n")
-        print(f"Total number of failures: {total_num_failures}\n")
-        print(
-            f"Average time per message sent: {overall_average_time_per_message}\n")
-        print("================================")
+        print(f"""
+              ================================
+              Report #{report_counter}
+              
+              Total number of successes: {total_num_successes}
+              Total number of failures: {total_num_failures}
+              Average time per message sent: {overall_average_time_per_message:.5f}
+              ================================
+              """)
         # Set a termination condition for recording so it won't record forever after all messages have been sent
         if total_num_successes == total_num_messages:
             return True
---
+++ test_monitor.py
@@ -0,0 +1,62 @@
+import io
+import unittest
+import sys
+from producer import Producer
+from sender import Sender
+from monitor import Monitor
+
+
+class TestMonitor(unittest.TestCase):
+
+    def setUp(self):
+        producer = Producer()
+        senders_list = [Sender(), Sender(), Sender()]
+        self.monitor = Monitor(1000, senders_list)
+
+    def test_get_update_interval(self):
+        self.assertEqual(self.monitor.get_update_interval(), 1000)
+
+    def test_get_update_interval(self):
+        self.assertEqual(self.monitor.get_update_interval(), 1000)
+
+    def test_set_update_interval_positive_integer(self):
+        self.monitor.set_update_interval(1234)
+        self.assertEqual(self.monitor.get_update_interval(), 1234)
+
+    def test_set_update_interval_invalid_value(self):
+        self.monitor.set_update_interval(-500)
+        self.assertEqual(self.monitor.get_update_interval(), 1000)
+
+    def test_record(self):
+        # test data for 3 Sender. Format: [success_counter, failure_counter, total_time] for each Sender
+        test_data = [[5, 2, 300], [8, 1, 400], [10, 0, 500]]
+        for i in range(3):
+            self.monitor.senders_list[i].success_counter = test_data[i][0]
+            self.monitor.senders_list[i].failure_counter = test_data[i][1]
+            self.monitor.senders_list[i].total_time = test_data[i][2]
+        self.assertTrue(self.monitor.record(1, 23))
+        # Reference: https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print
+        # Redirect stdout to capture print statement
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        self.monitor.record(2, 23)
+        # restore original stdout
+        sys.stdout = sys.__stdout__
+        # Remove leading/trailing whitespace
+        printed_content = captured_output.getvalue().strip()
+        expected_output = """
+              ================================
+              Report #2
+
+              Total number of successes: 23
+              Total number of failures: 3
+              Average time per message sent: 52.17391
+              ================================
+              """
+
+        # Remove leading/trailing whitespace
+        self.assertEqual(printed_content, expected_output.strip())
+
+
+if __name__ == '__main__':
+    unittest.main()
